d88e050633804c934e7d244fc18236a6e6be52f3
kseniakopli <55592072+kseniakopli@users.noreply.github.com>
2020-03-02 17:15:30 +0300

    add some fixes to routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import KanbanBoardContainer from "./components/KanbanBoardContainer";
 import CardForm from "./components/CardForm";
-import { Router, Route } from "react-router-dom";
-import createBrowserHistory from "history/createBrowserHistory";
+import { Router, Route, Switch } from "react-router-dom";
+import { createBrowserHistory } from "history";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
@@ -10,12 +10,14 @@ class App extends Component {
   render() {
     return (
       <div>
-      <Header/>
-      <Router history={createBrowserHistory()}>
-        <Route path="/" exact component={KanbanBoardContainer} />
-        <Route path="description/:id" component={CardForm} />
-      </Router>
-      <Footer/>
+        <Header />
+        <Router history={createBrowserHistory()}>
+          <Switch>
+            <Route path="/" exact component={KanbanBoardContainer} />
+            <Route path="/description/:id" component={CardForm} />
+          </Switch>
+        </Router>
+        <Footer />
       </div>
     );
   }
--- src/components/CardForm.js
+++ src/components/CardForm.js
@@ -1,32 +1,34 @@
 import React, { Component } from "react";
 import "./style.css";
 import { ReactComponent as Cross } from "../cross.svg";
+import cardList from "./cardList";
+import {Link} from 'react-router-dom';
 
 class CardForm extends Component {
-  handleClose(e) {
-    e.preventDefault();
-    this.props.handleClose();
-  }
-
   render() {
+    console.log(this);
+    let card = cardList.find(card => card.id == this.props.match.params.id);
     return (
       <main className="card-form">
-        <div className="card-form-header">
-          <div className="card-title">{this.props.title}</div>
-          <button onClick={this.handleClose.bind(this)}>
-            <Cross />
-          </button>
-        </div>
-        <div className="card-creation-date">01.01.2001</div>
-        <div className="card-description">
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
-          nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-          volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
-          ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
-          Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse
-          molestie consequat, vel illum dolore eu feugiat nulla facilisis at
-          vero eros et accumsan et iusto odio dignissim qui blandit praesent
-          luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+        <div className="card-detail">
+          <div className="card-form-header">
+            <div className="card-form-title">{card.title}</div>
+            <button className="btn icon-btn">
+            <Link to={"/"}><Cross /></Link>
+            </button>
+          </div>
+          <div className="card-creation-date">01.01.2001</div>
+          <div className="card-description">
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+            nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
+            volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+            ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
+            consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate
+            velit esse molestie consequat, vel illum dolore eu feugiat nulla
+            facilisis at vero eros et accumsan et iusto odio dignissim qui
+            blandit praesent luptatum zzril delenit augue duis dolore te feugait
+            nulla facilisi.
+          </div>
         </div>
       </main>
     );
